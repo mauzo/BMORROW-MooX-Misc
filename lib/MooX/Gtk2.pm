10b542e37a7c52f683e8e35d0fc16cb391f8c3ab
Ben Morrow <ben@morrow.me.uk>
2014-01-21 01:21:34 +0000

    Bind Moo attributes to gtk properties.
    This adds a 'gtk_prop' key to 'has', which binds the attribute to a
    property of a Gtk object (specified by an object path). If this key is
    specified an after modifier is applied to the attribute's _trigger method
    (which will be created if necessary) which sets the Gtk object's
    property. An after modifier is also applied to BUILD, which looks up the
    Gtk object, records it for the trigger, and connects to the 'notify'
    signal to synchronise in the other direction.

--- lib/MooX/Gtk2.pm
+++ lib/MooX/Gtk2.pm
@@ -4,11 +4,15 @@ use warnings;
 use strict;
 
 use Moo::Role;
+use MooX::AccessorMaker     qw/apply_accessor_maker_roles/;
+use MooX::CaptainHook       qw/on_application/;
+
 use MooX::MethodAttributes
     provide => [qw/ Signal Action /];
 
-use Carp            ();
-use Scalar::Util    ();
+use Carp                    ();
+use Hash::Util::FieldHash   ();
+use Scalar::Util            ();
 
 use namespace::clean;
 
@@ -18,6 +22,10 @@ with qw/
     MooX::NoGlobalDestruction 
 /;
 
+on_application {
+    my ($to, $from) = @{$_[0]};
+    apply_accessor_maker_roles $to, "MooX::Gtk2::AccessorMaker";
+};
 
 sub BUILD { }
 
@@ -60,6 +68,65 @@ after BUILD => sub {
         $act->signal_connect("activate",
             $self->weak_method($method));
     });
+};
+
+package MooX::Gtk2::AccessorMaker;
+
+use Moo::Role;
+
+around generate_method => sub {
+    my ($orig, $self, @args) = @_;
+    my ($into, $name, $spec) = @args;
+
+    my $pspec = $$spec{gtk_prop} or return $self->$orig(@args);
+    my ($path, $prop) = $pspec =~ /^(.*)::([\w-]+)$/
+        or Carp::croak("Bad property spec '$pspec'");
+
+    $$spec{trigger} = 1;
+
+    # this call will update $spec
+    my $methods = $self->$orig(@args);
+
+    my $reader  = $$spec{reader} // $$spec{accessor};
+    my $writer  = $$spec{writer} // $$spec{accessor};
+
+    $reader && $writer or Carp::croak("Attribute '$name' is not rw");
+
+    s/^\+//, s/^_// for $name;
+
+    my $trigger = "_trigger_$name";
+    my $mod     = \&Class::Method::Modifiers::install_modifier;
+
+    Hash::Util::FieldHash::fieldhash my %obj;
+
+    $into->can($trigger)
+        or $mod->($into, "fresh", $trigger, sub {});
+
+    $mod->($into, "after", $trigger, sub {
+        my ($self, $value) = @_;
+        my $obj = $obj{$self} or return;
+
+        $value eq $obj->get_property($prop) and return;
+        $obj->set_property($prop, $value);
+    });
+
+    $mod->($into, "after", "BUILD", sub {
+        my ($self) = @_;
+
+        my $obj = $obj{$self} = $self->_resolve_object_path($path);
+        Scalar::Util::weaken $obj{$self};
+
+        $obj->signal_connect("notify::$prop", 
+            $self->weak_closure(sub {
+                my ($self) = @_;
+                my $value = $obj{$self}->get_property($prop);
+                $value eq $self->$reader and return;
+                $self->$writer($value);
+            }));
+        $obj->set_property($prop, $self->$reader);
+    });
+
+    return $methods;
 };
 
 1;
