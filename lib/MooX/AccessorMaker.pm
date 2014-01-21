1e37bfa279b7fc631b0acfd5b41584b930857a03
Ben Morrow <ben@morrow.me.uk>
2014-01-21 01:12:24 +0000

    Cleaner access to the Moo accessor maker.
    Partly because Moo has no MOP, it doesn't provide any clean way to get
    at the accessor maker object for a given package. This module wraps code
    taken from MooX::LvalueAttribute, and at least provides a central point
    to update the hacks as necessary.

---
+++ lib/MooX/AccessorMaker.pm
@@ -0,0 +1,35 @@
+package MooX::AccessorMaker;
+
+use warnings;
+use strict;
+
+use Exporter "import";
+our @EXPORT = qw/find_accessor_maker apply_accessor_maker_roles/;
+
+sub find_accessor_maker {
+    my ($target) = @_;
+
+    if ($Moo::Role::INFO{$target} && 
+        $Moo::Role::INFO{$target}{is_role}
+    ) {
+        return 
+            $Moo::Role::INFO{$target}{accessor_maker} ||= do {
+                require Method::Generate::Accessor;
+                Method::Generate::Accessor->new;
+            };
+    }
+    elsif ($Moo::MAKERS{$target} && 
+        $Moo::MAKERS{$target}{is_class}
+    ) {
+        return Moo->_accessor_maker_for($target);
+    }
+}
+
+sub apply_accessor_maker_roles {
+    my ($target, @roles) = @_;
+
+    my $maker = find_accessor_maker($target);
+    Moo::Role->apply_roles_to_object($maker, @roles);
+}
+
+1;
