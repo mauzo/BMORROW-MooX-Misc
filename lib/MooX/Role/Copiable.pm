d5fc350249aeca6731af43b85571037c89a5789b
Ben Morrow <ben@morrow.me.uk>
2014-02-11 14:21:17 +0000

    Use Moo internals rather that BUILDARGS.
    This way I have access to the object we are building, which I will need
    if I want to implement deep copying.

--- lib/MooX/Role/Copiable.pm
+++ lib/MooX/Role/Copiable.pm
@@ -33,13 +33,6 @@ sub _find_copiable_atts_for {
     @roles;
 }
 
-sub _copy_init_args_for {
-    my ($self, $for, @roles) = @_;
-
-    map { my $r = $$_[1]; ($$_[3] => $self->$r) }
-    $self->_find_copiable_atts_for($for, @roles);
-}
-
 my $filter = sub {
     my ($item, $list) = @_;
     my @list = $list ? ref $list ? @$list : $list : ()
@@ -67,33 +60,6 @@ sub copy_from {
     }
 }
 
-# We have to supply the default BUILDARGS, since Moo::Object doesn't
-# have one.
-sub BUILDARGS {
-    my ($self, @args) = @_;
-    @args == 1 && ref $args[0] and return $args[0];
-    return { @args };
-}
-
-around BUILDARGS => sub {
-    my ($orig, $self, @args) = @_;
-
-    my $args = $self->$orig(@args);
-    #Carp::cluck "COPY BUILDARGS [$self]: " . pp $args;
-
-    my $from = delete $$args{copy_from} 
-        or return $args;
-    $from->DOES($Me) or croak "$from is not $Me";
-
-    my %copy = $from->_copy_init_args_for($self);
-    carp "COPY BUILDARGS [$self] FROM [$from]: " . pp [keys %copy];
-
-    +{
-        %copy,
-        %$args,
-    };
-};
-
 package MooX::Role::Copiable::Accessor;
 
 use Moo::Role;
@@ -103,12 +69,50 @@ after generate_method => sub {
 
     $$spec{copiable} or return;
 
-    push @{$COPIABLE_ATTS{$into}}, [
-        $name,
-        $$spec{reader} // $$spec{accessor},
-        $$spec{writer} // $$spec{accessor},
-        $$spec{init_arg} // $name,
-    ];
+    # This is something of a hack. We have to make the attribute
+    # unrequired, since we may be filling that requirement from a copy.
+    # In principle we can't affect the behaviour of the base object,
+    # since we're in an after modifier; in practice, changes to the spec
+    # hashref here will be picked up by the constructor code later.
+    my $reqd = delete $$spec{required};
+    $name =~ s/^\+//;
+
+    my $r = $$spec{reader} // $$spec{accessor};
+    my $w = $$spec{writer} // $$spec{accessor};
+
+    push @{$COPIABLE_ATTS{$into}}, [$name, $r, $w];
+    $$spec{_copiable_spec} = [$r, $into, $reqd];
+};
+
+# This is the method called to generate the code that copies values from
+# the BUILDARGS hash into the new object. This is the last point at
+# which we have access to copy_from, unless we make it a real attribute.
+around _generate_populate_set => sub {
+    my ($orig, $self, @args) = @_;
+    my ($me, $name, $spec, undef, $test, $init_arg) = @args;
+
+    my $default     = $self->$orig(@args);
+    my $copiable    = $$spec{_copiable_spec} or return $default;
+    my ($r, $role, $reqd) = @$copiable;
+
+    my $check_reqd  = $reqd ? qq{
+        $test or die "Missing required argument: \Q$init_arg\E";
+    } : "";
+        
+    qq{
+        { # BEGIN MooX::Role::Copiable [$name]
+            my \$from = \$args->{copy_from};
+            if (!$test
+                && \$from
+                && \$from->DOES("\Q$role\E")
+            ) {
+                \$args->{"\Q$init_arg\E"} = \$from->$r;
+            }
+        }
+        $check_reqd;
+        # END MooX::Role::Copiable [$name]
+        $default;
+    };
 };
 
 1;
