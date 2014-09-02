d0cd1c85e75da58023a03d5b67509eec6a81e6cd
Ben Morrow <ben@morrow.me.uk>
2014-09-02 22:06:06 +0100

    Meta.

--- lib/BMORROW/MooX/Misc.pm
+++ lib/BMORROW/MooX/Misc.pm
@@ -1,5 +1,11 @@
 package BMORROW::MooX::Misc;
 
+=head1 NAME
+
+BMORROW::MooX::Misc - Assorted small Moo extensions
+
+=cut
+
 use 5.010;
 use warnings;
 use strict;
@@ -7,3 +13,14 @@ use strict;
 our $VERSION = "0";
 
 1;
+
+=head1 AUTHOR
+
+Ben Morrow <ben@morrow.me.uk>
+
+=head1 COPYRIGHT
+
+Copyright 2014 Ben Morrow.
+
+Released under the 2-clause BSD licence.
+
